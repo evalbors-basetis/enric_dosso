0d4ee3da372bd9db66c80d4d9fde474ae809b990
Eva Albors <evaalbors@MacBook-Pro-de-Eva-Albors.local>
2021-04-16 18:10:50 +0200

    adding typography

--- js/carrousel.js
+++ js/carrousel.js
@@ -5,11 +5,19 @@ const rightArrow = document.querySelector(".right");
 const indicatorParents = document.querySelector(".controls ul");
 
 let sectionIndex = 0;
-// let sections = document.querySelector('.section');
+// let sections = document.querySelectorAll(".slider section");
 
 function removeSelected(){
   document.querySelector(".controls .selected").classList.remove("selected");
+
 }
+
+function removeTransitions(){
+  document.querySelector(".section .transition__left").classList.remove("transition__left");
+  // document.querySelector(".section .transition__right").classList.remove("transition__right");
+  // document.querySelector(".section .transition__left").classList.remove("transition__top");
+}
+
 
 document.querySelectorAll(".controls li").forEach(function (indicator, ind) {
   indicator.addEventListener("click", function () {
@@ -31,7 +39,7 @@ leftArrow.addEventListener("click", function () {
 
 rightArrow.addEventListener("click", function () {
   sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
-  removeSelected();
+  removeClasses();
   indicatorParents.children[sectionIndex].classList.add("selected");
   slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
   // añadir animación
