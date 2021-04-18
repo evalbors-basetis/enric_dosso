f40c9e0565ecbc213c750a9fe1a8b1ff4621579b
Eva Albors <evaalbors@MacBook-Pro-de-Eva-Albors.local>
2021-04-18 13:55:04 +0200

    making carrousel vanilla in new page

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,23 +1,13 @@
 const slider = document.querySelector(".slider");
-
 const leftArrow = document.querySelector(".left");
 const rightArrow = document.querySelector(".right");
 const indicatorParents = document.querySelector(".controls ul");
 
 let sectionIndex = 0;
-// let sections = document.querySelectorAll(".slider section");
 
 function removeSelected(){
   document.querySelector(".controls .selected").classList.remove("selected");
-
 }
-
-function removeTransitions(){
-  document.querySelector(".section .transition__left").classList.remove("transition__left");
-  // document.querySelector(".section .transition__right").classList.remove("transition__right");
-  // document.querySelector(".section .transition__left").classList.remove("transition__top");
-}
-
 
 document.querySelectorAll(".controls li").forEach(function (indicator, ind) {
   indicator.addEventListener("click", function () {
@@ -25,7 +15,6 @@ document.querySelectorAll(".controls li").forEach(function (indicator, ind) {
     removeSelected();
     indicator.classList.add("selected");
     slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
-    // añadir animación
   });
 });
 
@@ -34,13 +23,11 @@ leftArrow.addEventListener("click", function () {
   removeSelected();
   indicatorParents.children[sectionIndex].classList.add("selected");
   slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
-  // añadir animación
 });
 
 rightArrow.addEventListener("click", function () {
   sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
-  removeClasses();
+  removeSelected();
   indicatorParents.children[sectionIndex].classList.add("selected");
   slider.style.transform = "translate(" + sectionIndex * -25 + "%)";
-  // añadir animación
 });
