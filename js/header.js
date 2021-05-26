7b057d00a1c94b89f13404add6a14580a77be5f1
Eva Albors <evaalbors@MacBook-Pro-de-Eva-Albors.local>
2021-05-26 12:27:03 +0200

    adding inside pages

--- js/header.js
+++ js/header.js
@@ -2,21 +2,9 @@ class MyHeader extends HTMLElement {
   constructor() {
     super();
     this.innerHTML = `
-  <header>
-    <div class="logo-center">
-      <a href="./index.html"><img src="images/logo-responsive.svg" alt="logotipo" width="30" height="auto"></a>
-    </div>
-    <div>
-      <ul class="menu">
-        <li><a class="menu-selected" href="./1_vinebre_v2.html">Vinebre</a></li>
-        <li><a href="">Tortosa</a></li>
-        <li><a href="">Monteserrat</a></li>
-        <li><a href="">Tarragona</a></li>
-        <li><a href="">Barcelona</a></li>
-        <li><a href="">Castellón</a></li>
-      </ul>
-    </div>
-  </header>
+    <header>
+    
+    </header>
   `
   ;
   }
